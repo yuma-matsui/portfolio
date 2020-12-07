2d73321dea8a1bbc7636953899594fb81701503a
yuma-matsui <oshino.iiyo@gmail.com>
2020-12-07 14:26:21 +0900

    見出し部分のアニメーション設定

---
+++ app/javascript/packs/animation_title.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const ta = new TextAnimation('.animate-title');
+  ta.animate();
+});
+
+
+class TextAnimation{
+  constructor(el){
+    this.el = document.querySelector(el);
+    this.chars = this.el.innerHTML.trim().split('');
+    this.el.innerHTML = this._splitText();
+  }
+  _splitText(){
+    return this.chars.reduce((acc, curr) =>{
+      curr = curr.replace(' ', '&nbsp;')
+      return `${acc}<span class="char">${curr}</span>`
+    }, "");
+  }
+  animate(){
+    this.el.classList.toggle('inview');
+  }
+}
