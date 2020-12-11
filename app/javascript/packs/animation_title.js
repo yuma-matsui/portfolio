857daeb03eaa2b8d53ac5e47d568a4306de9988a
yuma-matsui <oshino.iiyo@gmail.com>
2020-12-11 10:40:09 +0900

    ローダーとJSアニメーション発火タイミングの調整

--- app/javascript/packs/animation_title.js
+++ app/javascript/packs/animation_title.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   const ta = new TweenTextAnimation('.tween-animate-title');
-  ta.animate();
+  Pace.on('done', ta.animate.bind(ta));
 });
 
 
